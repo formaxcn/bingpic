6df3d702aa5d1ac7cf4df2298b22780e67bf2dd5
tjrtzh <tjrtzh@gmail.com>
2015-08-30 09:31:31 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/index.php

--- 1/index.php
+++ 1/index.php
@@ -1,6 +1,7 @@
 <?php  
-	$randseed=rand(1,100);
-	echo $randseed;
+	$randseed=1;
+//$randseed=rand(1,100);
+//echo $randseed;
     $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx='.$randseed.'&n=1');  
     if(preg_match("/<url>(.+?)<\/url>/ies",$str,$matches)){  
         $imgurl='http://cn.bing.com'.$matches[1];  
