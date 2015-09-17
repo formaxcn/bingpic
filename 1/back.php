021a8e11399b604491194e1f7e323308200d326b
TangZhihao <tjrtzh@gmail.com>
2015-09-17 20:11:35 +0800

    Update back.php

--- 1/back.php
+++ 1/back.php
@@ -1,8 +1,8 @@
 <?php  
 	$randseed=rand(0,18);
-    $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx='.$randseed.'&n=1');
+    $str=file_get_contents('http://www.bing.com/HPImageArchive.aspx?format=js&idx='.$randseed.'&n=1');
     $array = json_decode($str);
-	$imgurl = 'http://s.cn.bing.net'.$array->{"images"}[0]->{"urlbase"}.'_1920x1080.jpg';
+	$imgurl = 'http://www.bing.com'.$array->{"images"}[0]->{"urlbase"}.'_1920x1080.jpg';
 	
     if($imgurl){  
         header('Content-Type: image/JPEG');  
