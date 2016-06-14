a22e5701f54675acbd8fd61e01e0c09d726008bf
Zhihao Tang <tjrtzh@gmail.com>
2016-06-14 07:59:05 +0000

    short url test

--- 1/back.php
+++ 1/back.php
@@ -1,16 +1,3 @@
 <?php  
-	$randseed=rand(0,18);
-    $str=file_get_contents('http://www.bing.com/HPImageArchive.aspx?format=js&idx='.$randseed.'&n=1');
-    $array = json_decode($str);
-	$imgurl = 'http://www.bing.com'.$array->{"images"}[0]->{"urlbase"}.'_1920x1080.jpg';
-	
-    if($imgurl){  
-        header('Content-Type: image/JPEG');  
-        @ob_end_clean();  
-        @readfile($imgurl);  
-        @flush(); @ob_flush();  
-        exit();  
-    }else{  
-        exit('pic error ');  
-    }  
+	echo "hello"
 ?> 
