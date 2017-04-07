0909cd244102d90f2afd4abd3961ce7b45cc2b2e
Tang Zhihao <tjrtzh@gmail.com>
2017-04-07 11:08:55 +0800

    ssl

--- 1/index.php
+++ 1/index.php
@@ -1,8 +1,8 @@
 <?php  
 	$randseed=rand(0,18);
-    $str=file_get_contents('http://www.bing.com/HPImageArchive.aspx?format=js&mkt=en-US&idx='.$randseed.'&n=1');
+    $str=file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&mkt=en-US&idx='.$randseed.'&n=1');
     $array = json_decode($str);
-	$imgurl = 'http://www.bing.com'.$array->{"images"}[0]->{"urlbase"}.'_1920x1080.jpg';
+	$imgurl = 'https://www.bing.com'.$array->{"images"}[0]->{"urlbase"}.'_1920x1080.jpg';
 	
     if($imgurl){  
         header('Content-Type: image/JPEG');  
